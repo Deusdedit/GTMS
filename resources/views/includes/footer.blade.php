78157a7b7de46bab914db55b86e5be1d5099defc
TUMAINI STEVEN <42651024+octrinho@users.noreply.github.com>
2021-02-26 11:02:02 +0300

    footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,8 +1,5 @@
-
-<footer class="main-footer" style="">
-  <strong>Copyright &copy; 2021 <a href="https://www.gst.go.tz/">GST</a>.</strong>
+  <strong>Copyright &copy; 2021 <a href="https://www.gst.go.tz/">GSTvfr</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0.0
     </div>
-</footer>
