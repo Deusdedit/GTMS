d9c192d3dc1a080ad72ddf7d7ecfbbd24c7b2abb
TUMAINI STEVEN <42651024+octrinho@users.noreply.github.com>
2021-02-26 14:28:20 +0300

    colors

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,10 +5,10 @@
     </head>
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
-<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+<nav class="main-header navbar navbar-expand navbar-white navbar-light" style="background-color:#E1BA47;">
     @include('includes.header')
   </nav>
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <aside class="main-sidebar sidebar-dark-primary elevation-4" >
     @include('includes.sidebar')
   </aside>
 
@@ -21,7 +21,7 @@
       </section>
   </div>
 
-  <footer class="main-footer">
+  <footer class="main-footer" style="background-color:#E1BA47;">
     @include('includes.footer')
   </footer>
 @include('includes.footer_scripts')
