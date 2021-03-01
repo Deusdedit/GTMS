e0ed3d11d0cc18ca37c0486e28e023d86f1215b3
TUMAINI STEVEN <42651024+octrinho@users.noreply.github.com>
2021-03-01 11:14:44 +0300

    assigned task

--- resources/views/welcomes/employee_welcome.blade.php
+++ resources/views/welcomes/employee_welcome.blade.php
@@ -21,11 +21,25 @@
                 <!-- /.col -->
                 <div class="col-md-3 col-sm-6 col-12">
                     <div class="info-box">
-                    <span class="info-box-icon bg-info"><i class="fas fa-spinner"></i></span>
+                    <span class="info-box-icon bg-warning"><i class="fas fa-spinner"></i></span>
 
                     <div class="info-box-content">
                         <span class="info-box-text">Ongoing Tasks</span>
                         <span class="info-box-number">{{$ongotask}}</span>
+                    </div>
+                    <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+
+                <div class="col-md-3 col-sm-6 col-12">
+                    <div class="info-box">
+                    <span class="info-box-icon bg-info"><i class="fas fa-tasks"></i></span>
+
+                    <div class="info-box-content">
+                        <span class="info-box-text">Assigned Task</span>
+                        <span class="info-box-number">++</span>
                     </div>
                     <!-- /.info-box-content -->
                     </div>
