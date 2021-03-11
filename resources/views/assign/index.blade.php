38d5eced0e334dc47a9064df1b8ee5b1d8740ed8
Deusdedit <dbyabatov@gmail.com>
2021-03-11 08:44:16 +0300

    Update index.blade.php

--- resources/views/assign/index.blade.php
+++ resources/views/assign/index.blade.php
@@ -5,10 +5,11 @@
 <div class="card">
     <div class="card-header"> 
     <h3 class="card-title">All assigned Activities </h3>
-    <a href="{{route('printReportActivityAssigned')}}" target="_blank">
+    <!-- <a href="{{route('printReportActivityAssigned')}}" target="_blank">
     <button type="button" class="btn btn-success btn-sm" style="float:right" ><i class="fas fa-print"></i> Print </button>
-    </a>
+    </a> -->
     <button type="button" class="btn btn-primary btn-sm" style="float:right; margin-right:10px;" data-toggle="modal" data-target="#modal-activity">Assign Activity</button>
+    <!-- <button type="button" class="btn btn-info btn-sm" style="float:right; margin-right:10px;" data-toggle="modal" data-target="#modal-activityGroup">Assign Group Activity</button> -->
     </div>
     <!-- /.card-header -->
     <div class="card-body">
@@ -101,8 +102,100 @@
     <!-- /.card-body -->
 </div>
 
-<!-- add new activity -->
+<!-- assign individual activity -->
 <div class="modal fade" id="modal-activity">
+    <form role="form" method="post" action="{{ route('assign.store') }}" id="activityForm">
+        @csrf
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+            
+                <div class="modal-header">
+                    <h4 class="modal-title">Assign activity</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Select User</label>
+                        <select class="form-control select2" style="width: 100%;" name="user_id" placeholder="Select a user....">
+                            <option selected="selected" disabled>Full name of Department (Section)...</option>
+                                @foreach($users as $user)
+                                    @foreach($sections as $section)
+                                        @if($section->id == $user->section_id)
+                                            @foreach($departments as $department)
+                                                @if($department->id == $section->department_id)
+                                                <option value="{{$user->id}}">{{$user->first_name}} {{$user->middle_name}} {{$user->last_name}} of <i> {{$department->name}} ({{$section->name}}).</i> </option>
+                                                @endif
+                                            @endforeach
+                                        @endif
+                                    @endforeach
+                                @endforeach
+                        </select>
+                    </div>
+                    <div class="row">
+                        
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+                                <label for="ledgerNumberId">Activity Name</label>
+                                <input type="text" class="form-control" id="ledgerNumberId" placeholder="Enter activity name" name="name">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="quantityId">Client</label>
+                                <input type="text" class="form-control" id="quantityId" placeholder="Enter Client name" name="client">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="costId">Activity Details</label>
+                                <input type="text" class="form-control" id="costId" placeholder="Enter activity details" name="details">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="itemNameId">Date to start activity</label>
+                                <input type="date" class="form-control" id="itemNameId" placeholder="Enter Date to start " name="start_assign_date">
+                            </div>
+
+                        </div>
+
+                        <div class="col-6">
+                            <div class="form-group">
+                                <label for="itemNameId">Colaborators</label>
+                                <input type="text" class="form-control" id="itemNameId" placeholder="Enter colaborators names" name="colaborators">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="supplierId">Expected Output</label>
+                                <input type="text" class="form-control" id="supplierId" placeholder="Enter expected output" name="output">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="totalcostId">Resources</label>
+                                <input type="text" class="form-control" id="totalcostId" placeholder="Enter resources" name="resources">
+                            </div>
+                            <div class="form-group">
+                                <label for="itemNameIdd">Duration in Days</label>
+                                <input type="number" class="form-control" id="itemNameIdd" placeholder="Enter activity duration " name="duration">
+                            </div>
+
+                        </div>   
+                    </div>    
+                </div>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Assign activity</button>
+                </div>
+                
+            </div>
+            <!-- /.modal-content -->
+        </div>
+    </form>
+    <!-- /.modal-dialog -->
+</div>
+
+<!-- assign group activity -->
+<div class="modal fade" id="modal-activityGroup">
     <form role="form" method="post" action="{{ route('assign.store') }}" id="activityForm">
         @csrf
         <div class="modal-dialog modal-lg">
